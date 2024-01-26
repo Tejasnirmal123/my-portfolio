6a19cdc128c02ac8c2c9831943c1e11b102b7c29
Tejas Nirmal <102731333+Tejasnirmal123@users.noreply.github.com>
2024-01-26 13:42:58 +0530

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.homePage,name="home"),
+    path('',views.homePage,name='home'),
     path("contact/",views.contact,name='contact'),
-    path('project/<str:pk>/',views.projectPage,name="project"),
-    path('add-project/',views.addProject,name="add-project"),
-    path('edit-project/<str:pk>/',views.editProject,name="edit-project"),
+    path('project/<str:pk>/',views.projectPage,name='project'),
+    path('add-project/',views.addProject,name='add-project'),
+    path('edit-project/<str:pk>/',views.editProject,name='edit-project'),
     path("chart/",views.chartPage,name='chart'),
-    path('add-skill/',views.addSkill,name="add-skill"),
+    path('add-skill/',views.addSkill,name='add-skill'),
 
 
 ]
