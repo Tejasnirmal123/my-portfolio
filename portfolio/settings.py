93a40399deafed666c08351e3726999ce4620a64
Tejas Nirmal <102731333+Tejasnirmal123@users.noreply.github.com>
2024-01-26 14:57:13 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -63,9 +63,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'templates'
-        ],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
                 'context_processors': [
